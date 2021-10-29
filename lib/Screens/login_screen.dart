b74b0f6abce80cc8ac206c70b549ec9f88596e3f
Sarthak Agarwal <agarwalsarthak456@gmail.com>
2021-10-30 01:55:07 +0530

    Improved Login Screen UI

--- lib/Screens/login_screen.dart
+++ lib/Screens/login_screen.dart
@@ -2,9 +2,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:pro_share/Provider/user_data.dart';
 import 'package:pro_share/Services/google_auth_api.dart';
-import 'package:provider/provider.dart';
 
 import 'home_screen.dart';
 
@@ -14,18 +12,11 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: () {
-      //     print(Provider.of<UserData>(context, listen: false).userPhotoUrl);
-      //   },
-      // ),
       backgroundColor: Colors.black,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
+      body: Stack(
+        children: [
+          Center(
+            child: Text(
               'ProShare',
               style: GoogleFonts.poppins(
                 color: Colors.white,
@@ -33,10 +24,16 @@ class LoginScreen extends StatelessWidget {
                 fontSize: 40,
               ),
             ),
-            const SizedBox(
-              height: 150,
-            ),
-            TextButton(
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: TextButton(
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                    const Color(0xff0276E8),
+                  ),
+                  padding: MaterialStateProperty.all(
+                      const EdgeInsets.symmetric(vertical: 8, horizontal: 35))),
               onPressed: () async {
                 final user = await GoogleAuthApi().loginWithGoogle(context);
                 if (user) {
@@ -44,18 +41,63 @@ class LoginScreen extends StatelessWidget {
                       MaterialPageRoute(builder: (_) => const HomeScreen()));
                 }
               },
-              child: Text(
-                'Login',
-                style: GoogleFonts.poppins(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w600,
-                  fontSize: 18,
-                ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(15.0)),
+                    child: Image.network(
+                      "https://cdn.icon-icons.com/icons2/2631/PNG/512/google_search_new_logo_icon_159150.png",
+                      height: 30,
+                      width: 30,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  Text(
+                    'Login',
+                    style: GoogleFonts.poppins(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
               ),
-            )
-          ],
-        ),
+            ),
+          ),
+          buildRotatedWidget("reddit", 50, 50, 40),
+          buildRotatedWidget("reddit", MediaQuery.of(context).size.height - 90,
+              MediaQuery.of(context).size.width - 90, 40),
+          buildRotatedWidget("twitter", 450, 350, 37),
+          buildRotatedWidget("insta", 650, 110, 78),
+          buildRotatedWidget("Linkedin", 270, 18, 12),
+          buildRotatedWidget("snapchat", 170, 200, 31),
+          buildRotatedWidget(
+              "messenger", 55, MediaQuery.of(context).size.width - 90, 31),
+          buildRotatedWidget("reddit", 50, 50, 312),
+          buildRotatedWidget("facebook", MediaQuery.of(context).size.width + 75,
+              MediaQuery.of(context).size.height * 0.1, 315),
+        ],
       ),
     );
   }
+
+  Widget buildRotatedWidget(
+          String image, double top, double left, double tilt) =>
+      Positioned(
+        top: top,
+        left: left,
+        child: RotationTransition(
+          turns: AlwaysStoppedAnimation(tilt / 360),
+          child: Image.asset(
+            "Assets/Images/$image.png",
+            height: 50,
+            width: 50,
+          ),
+        ),
+      );
 }
